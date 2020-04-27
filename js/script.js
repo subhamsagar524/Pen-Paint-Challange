bd5020ada633a885dddd74e6a618c17f41cea449
subhamsagar524 <my.subham524@gmail.com>
2020-04-27 15:35:54 +0530

    first stage

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var el = document.getElementById("mycanvas");
+var ctx = el.getContext("2d");
+var isDrawing;
+
+el.onmousedown = function(e) {
+	  isDrawing = true;
+	  ctx.moveTo(e.clientX, e.clientY);
+};
+el.onmousemove = function(e) {
+	  if (isDrawing) {
+		      ctx.lineTo(e.clientX, e.clientY);
+		      ctx.stroke();
+		    }
+};
+el.onmouseup = function() {
+	  isDrawing = false;
+};
+
